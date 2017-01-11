3c4414a84cc63a2cace75fb1d6f7d94161fc863c
Daniel Slone <slonej3@nku.edu>
2017-01-10 19:21:41 -0500

    first commit

---
+++ Employee.rb
@@ -0,0 +1,13 @@
+class Employee
+  # constructor method
+  #STATE,PLANT,DEPT,EMPID,NAME,COUNT
+  attr_accessor :state, :dept, :name, :count, :plant, :empid
+
+  def initialize(st, plant, dept, empid, name, count)
+    @state, @plant, @dept, @empid, @name, @count = st, plant, dept, empid, name, count
+  end
+
+  # def getState
+  #   @state
+  # end
+end
---
+++ widgets.rb
@@ -0,0 +1,14 @@
+require "./Employee"
+
+class Widgets
+  puts "hello"
+  # puts File.read("widgets.csv")
+
+  File.open("widgets.csv", "r") do |f|
+    f.each_line do |line|
+      splitLine = line.split(',')
+      puts splitLine[0]
+      em = Employee.new(splitLine[0], splitLine[1], splitLine[2], splitLine[3], splitLine[4], splitLine[5])
+    end
+  end
+end
